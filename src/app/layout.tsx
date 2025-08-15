d7348ec33d573c0aa8e02d0bc197a96cb1f71505
Mayank Bisht <octpahadi@gmail.com>
2025-08-15 20:03:19 +0530

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ const geistMono = Geist_Mono({
 const poppins = Poppins({
   variable: "--font-poppins",
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["devanagari"]
 });
 
 export const metadata: Metadata = {
